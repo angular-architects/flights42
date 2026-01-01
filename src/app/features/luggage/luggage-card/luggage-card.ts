059c98f39a16850a8814a5b9a48bf6e210648a3f
Manfred Steyer <manfred.steyer@gmx.net>
2026-01-01 21:23:54 +0100

    feat: implement live cycle hooks in luggage card

--- src/app/features/luggage/luggage-card/luggage-card.ts
+++ src/app/features/luggage/luggage-card/luggage-card.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  output,
+  SimpleChanges,
+} from '@angular/core';
 import { Luggage } from '../../../data/luggage';
 import { CityPipe } from '../../../shared/city.pipe';
 
@@ -8,10 +17,22 @@ import { CityPipe } from '../../../shared/city.pipe';
   templateUrl: './luggage-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LuggageCard {
+export class LuggageCard implements OnInit, OnChanges, OnDestroy {
   item = input.required<Luggage>();
   selected = input(false);
   selectedChange = output<boolean>();
+
+  ngOnInit(): void {
+    console.log('OnInit', this.item());
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges', changes);
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy', this.item());
+  }
 
   select() {
     this.selectedChange.emit(true);
