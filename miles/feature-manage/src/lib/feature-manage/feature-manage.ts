dfa44112f44c1f11a8e7039b6a4b3817e48e3651
Manfred Steyer <manfred.steyer@gmx.net>
2026-01-07 18:19:47 +0100

    chore: add miles app and libs

---
+++ miles/feature-manage/src/lib/feature-manage/feature-manage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-feature-manage',
+  imports: [],
+  templateUrl: './feature-manage.html',
+  styleUrl: './feature-manage.css',
+})
+export class FeatureManage {}
