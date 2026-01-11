41728eaf160d7b382f14b69118df23cab9fc89f9
Manfred Steyer <manfred.steyer@gmx.net>
2026-01-11 20:44:17 +0100

    feat: add proto flight search with data access

---
+++ src/app/domains/ticketing/feature-booking/proto-flight-search-02/flight-search.ts
@@ -0,0 +1,52 @@
+import { DatePipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Flight } from '../../data/flight';
+
+@Component({
+  selector: 'app-flight-search',
+  imports: [FormsModule, DatePipe, JsonPipe],
+  templateUrl: './flight-search.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FlightSearch {
+  private http = inject(HttpClient);
+
+  protected readonly from = signal('Graz');
+  protected readonly to = signal('Hamburg');
+
+  protected readonly flights = signal<Flight[]>([]);
+  protected readonly selectedFlight = signal<Flight | null>(null);
+
+  protected search(): void {
+    const url = 'https://demo.angulararchitects.io/api/flight';
+    const params = {
+      from: this.from(),
+      to: this.to(),
+    };
+
+    const headers = {
+      accept: 'application/json',
+    };
+
+    this.http.get<Flight[]>(url, { params, headers }).subscribe({
+      next: (flights) => {
+        this.flights.set(flights);
+      },
+      error: (error) => {
+        console.error('error loading flights', error);
+      },
+    });
+  }
+
+  protected select(flight: Flight): void {
+    this.selectedFlight.set(flight);
+  }
+}
