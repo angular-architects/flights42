405eba678044559e3c66f67cd7a3d6c94ffea67a
Manfred Steyer <manfred.steyer@gmx.net>
2026-02-06 16:09:03 +0100

    chore: rename flight store simple

--- src/app/domains/ticketing/feature-booking/passenger-search/passenger-search.ts
+++ src/app/domains/ticketing/feature-booking/passenger-search/passenger-search.ts
@@ -41,7 +41,8 @@ export class PassengerSearch {
 
   constructor() {
     this.showError();
-    this.store.updateFilter(this.filter);
+
+    // TODO: Call updateFilter to connect the filter to the rxMethod
   }
 
   private showError() {
@@ -55,8 +56,7 @@ export class PassengerSearch {
   }
 
   protected search(): void {
-    this.store.updateFilter(this.filter());
-    // this.store.reload();
+    // TODO: Call updateFilter to reload data with the _current_ parameters
   }
 
   protected updateSelected(passengerId: number, selected: boolean): void {
