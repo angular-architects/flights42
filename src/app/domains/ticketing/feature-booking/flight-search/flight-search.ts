68cc4ab570d4755053eed661666b15117a79fd68
Manfred Steyer <manfred.steyer@gmx.net>
2026-02-08 18:29:00 +0100

    feat: prepare starter kit

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
@@ -3,16 +3,18 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
-  effect,
   inject,
   signal,
 } from '@angular/core';
-import { debounce, form, FormField } from '@angular/forms/signals';
+import { form, FormField } from '@angular/forms/signals';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterLink } from '@angular/router';
 
+<<<<<<< HEAD
 // import { LuggageClient } from '../../../luggage/data/luggage-client';
 import { appSettings } from '../../../shared/util-common/app-settings';
+=======
+>>>>>>> bd5de5c (feat: prepare starter kit)
 import { Flight } from '../../data/flight';
 import { FlightClient } from '../../data/flight-client';
 import { FlightCard } from '../../ui/flight-card/flight-card';
@@ -32,20 +34,15 @@ export class FlightSearch {
     to: 'Hamburg',
   });
 
-  protected readonly filterForm = form(this.filter, (path) => {
-    debounce(path.from, appSettings.debounceTimeMs);
-    debounce(path.to, appSettings.debounceTimeMs);
-  });
-  protected readonly delayInMin = signal(0);
+  // TODO: add deboncing
+  protected readonly filterForm = form(this.filter);
 
-  protected readonly flightsResource = this.flightClient.findResource(
-    this.filterForm.from().value,
-    this.filterForm.to().value,
-  );
+  // TODO: create flightsResource
 
-  protected readonly flights = this.flightsResource.value;
-  protected readonly isLoading = this.flightsResource.isLoading;
-  protected readonly error = this.flightsResource.error;
+  // TODO: get signals from resource
+  protected readonly flights = signal<Flight[]>([]);
+  protected readonly isLoading = signal(false);
+  protected readonly error = signal<Error | null>(null);
 
   protected readonly basket = signal<Record<number, boolean>>({});
 
@@ -53,15 +50,29 @@ export class FlightSearch {
     () => this.filter().from + ' - ' + this.filter().to,
   );
 
-  protected readonly flightsWithDelay = computed(() =>
-    toFlightsWithDelays(this.flights(), this.delayInMin()),
-  );
+  // TODO: add signal delayInMin with initial value 0
+
+  // TODO: compute flights with delays
 
   constructor() {
+    // TODO: add an effect that shows a snackbar with an error message
   }
 
   protected search(): void {
-    this.flightsResource.reload();
+    // TODO: just reload the resource instead of calling the client directly
+    this.isLoading.set(true);
+    this.error.set(null);
+
+    this.flightClient.find(this.filter().from, this.filter().to).subscribe({
+      next: (flights) => {
+        this.flights.set(flights);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        this.error.set(error);
+        this.isLoading.set(false);
+      },
+    });
   }
 
   protected updateBasket(flightId: number, selected: boolean): void {
@@ -72,7 +83,8 @@ export class FlightSearch {
   }
 
   protected delay(): void {
-    this.delayInMin.update((delay) => delay + 15);
+    // TODO: add 15 to delayInMin instead of directly updating flights
+    this.flights.update((flights) => toFlightsWithDelays(flights, 15));
   }
 }
 
