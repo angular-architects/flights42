6a90e986e0359640aa5020f271664575a187f0d4
Manfred Steyer <manfred.steyer@gmx.net>
2026-02-09 16:33:02 +0100

    fix: remove solution

--- src/app/domains/luggage/feature-luggage/luggage-overview/luggage-overview.ts
+++ src/app/domains/luggage/feature-luggage/luggage-overview/luggage-overview.ts
@@ -15,23 +15,15 @@ import { LuggageStore } from './luggage-store';
 })
 export class LuggageOverview {
   private readonly store = inject(LuggageStore);
-<<<<<<< HEAD
-  private readonly dispatch = injectDispatch(luggageEvents);
-=======
 
   // TODO: inject Dispatcher for Signal Store Event API
->>>>>>> acd41f4 (chore: rename flight store simple)
 
   protected readonly luggage = this.store.luggage;
   protected readonly selected = this.store.selected;
 
   constructor() {
-<<<<<<< HEAD
-    this.dispatch.loadLuggageTriggered();
-=======
+
     // TODO: dispatch event to load luggage
-    // this.dispatcher.dispatch(luggageEvents.loadLuggage());
->>>>>>> acd41f4 (chore: rename flight store simple)
   }
 
   protected updateSelected(luggageId: number, selected: boolean): void {
