6c22deb57d12ee2673afc68a97b2c2b8589bc48b
Manfred Steyer <manfred.steyer@gmx.net>
2026-02-09 16:55:11 +0100

    chore: remove unnecessary import

--- src/app/domains/ticketing/feature-booking/advanced-flight-edit/flight-form/flight-form.ts
+++ src/app/domains/ticketing/feature-booking/advanced-flight-edit/flight-form/flight-form.ts
@@ -1,19 +1,13 @@
 import { Component, input } from '@angular/core';
 import { FieldTree, FormField } from '@angular/forms/signals';
 
-import { DateControl } from '../../../../shared/ui-common/date-control/date-control';
 import { FieldMetaDataPane } from '../../../../shared/ui-forms/field-meta-data-pane/field-meta-data-pane';
 import { ValidationErrorsPane } from '../../../../shared/ui-forms/validation-errors/validation-errors-pane';
 import { Flight } from '../../../data/flight';
 
 @Component({
   selector: 'app-flight',
-  imports: [
-    FormField,
-    ValidationErrorsPane,
-    FieldMetaDataPane,
-    DateControl,
-  ],
+  imports: [FormField, ValidationErrorsPane, FieldMetaDataPane],
   templateUrl: './flight-form.html',
 })
 export class FlightForm {
