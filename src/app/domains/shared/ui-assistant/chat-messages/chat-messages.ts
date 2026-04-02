aa078d72c59597c66c6ce2c970f285ff17f13690
Manfred Steyer <manfred.steyer@gmx.net>
2026-04-02 02:34:23 +0200

    feat: only accpt tool calls as answer

--- src/app/domains/shared/ui-assistant/chat-messages/chat-messages.ts
+++ src/app/domains/shared/ui-assistant/chat-messages/chat-messages.ts
@@ -63,9 +63,7 @@ export class ChatMessages {
         typeof message.content === 'string' ? message.content : String(''),
       hasContent: this.hasContent(message),
       icon: this.icons[message.role] || '❓',
-      toolCalls: message.toolCalls.filter(
-        (toolCall) => true, //toolCall.name !== 'showComponent',
-      ),
+      toolCalls: message.toolCalls,
     })),
   );
 }
