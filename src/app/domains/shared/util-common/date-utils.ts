155282ec094aff038bdaf0107dfa10557266e8c3
Manfred Steyer <manfred.steyer@gmx.net>
2026-04-16 22:27:16 +0200

    refactor: action cards

--- src/app/domains/shared/util-common/date-utils.ts
+++ src/app/domains/shared/util-common/date-utils.ts
@@ -1,3 +1,7 @@
+import { format } from 'date-fns';
+
+const UI_DATE_TIME_FORMAT = 'dd.MM.yyyy HH:mm';
+
 export function toLocalDateTimeString(isoString: string): string {
   const pad = (n: number) => n.toString().padStart(2, '0');
 
@@ -15,4 +19,13 @@ export function toLocalDateTimeString(isoString: string): string {
     ':' +
     pad(date.getMinutes())
   );
+}
+
+export function formatUiDateTime(value: string): string {
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) {
+    return value;
+  }
+
+  return format(date, UI_DATE_TIME_FORMAT);
 }
