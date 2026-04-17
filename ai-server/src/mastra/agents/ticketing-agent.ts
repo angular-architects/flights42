bf5209ec1cdce46f9153fb9c3b3da54fc05f9d11
Manfred Steyer <manfred.steyer@gmx.net>
2026-04-18 01:31:01 +0200

    feat: add mcp apps

--- ai-server/src/mastra/agents/ticketing-agent.ts
+++ ai-server/src/mastra/agents/ticketing-agent.ts
@@ -1,30 +1,27 @@
 import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
 
-// import { initMcpServer } from '../../../../libs/ag-ui-server/index.js';
-import { model } from '../config.js';
+import { initMcpServer } from '../../../../libs/ag-ui-server/index.js';
 import { bookFlightTool } from '../tools/book-flight.js';
 import { cancelFlightTool } from '../tools/cancel-flight.js';
 import { findBookedFlightsTool } from '../tools/find-booked-flights.js';
-// import { packageAgent } from './package-agent.js';
 import { ticketingAgentPrompt } from './ticketing-agent.prompt.js';
 
-// const hotelsMcpTools = await initMcpServer({
-//   serverId: 'hotels',
-//   url: new URL('http://127.0.0.1:3002/mcp'),
-// });
+const hotelsMcpTools = await initMcpServer({
+  serverId: 'hotels',
+  url: new URL('http://127.0.0.1:3002/mcp'),
+});
 
 export const ticketingAgent = new Agent({
   id: 'ticketingAgent',
   name: 'Flight42 Ticketing Assistant',
   instructions: ticketingAgentPrompt,
-  model,
+  model: 'openai/gpt-5.3-chat-latest',
   tools: {
     findBookedFlightsTool,
     bookFlightTool,
     cancelFlightTool,
-    // ...hotelsMcpTools,
+    ...hotelsMcpTools,
   },
-  // agents: { packageAgent },
   memory: new Memory(),
 });
