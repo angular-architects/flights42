b3fe0d436ab542ecac041f08b6153abbeb661c80
Manfred Steyer <manfred.steyer@gmx.net>
2026-06-23 12:39:34 +0200

    fix: use gpt mini for workflow

--- ai-server/src/mastra/agents/travel-refinement-agent.ts
+++ ai-server/src/mastra/agents/travel-refinement-agent.ts
@@ -5,13 +5,12 @@ import { model, modelAdvancedTasks } from '../config.js';
 import { findHotelsTool } from '../tools/find-hotels.js';
 import { searchFlightsTool } from '../tools/search-flights.js';
 import { travelRefinementAgentPrompt } from './travel-refinement-agent.prompt.js';
-import { OpenAILanguageModelResponsesOptions } from '@ai-sdk/openai';
 
 export const travelRefinementAgent = new Agent({
   id: 'travelRefinementAgent',
   name: 'Flight42 Travel Refinement',
   instructions: travelRefinementAgentPrompt,
-  model: modelAdvancedTasks,
+  model: model,
   tools: { searchFlightsTool, findHotelsTool },
   memory: new Memory(),
 });
