740a051b935282cd0c846c9c81f5cdb8f644ac51
Manfred Steyer <manfred.steyer@gmx.net>
2026-07-06 22:43:43 +0200

    chore: remove Hashbrown integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHashbrown } from '@hashbrownai/angular';
 import { MCP_APPS_SERVER_URL, provideMcpApps } from '@internal/ag-ui-client';
 import { provideMarkdown } from 'ngx-markdown';
 
@@ -17,18 +16,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAppInitializer(() => inject(ConfigService).load()),
-    // provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
-    provideHashbrown({
-      baseUrl: 'http://localhost:3000/api/chat',
-      emulateStructuredOutput: true,
-      middleware: [
-        (request) => {
-          console.log('[Hashbrown Request]', request);
-          return request;
-        },
-      ],
-    }),
     {
       provide: MCP_APPS_SERVER_URL,
       useFactory: () => inject(ConfigService).mcpServerUrl,
