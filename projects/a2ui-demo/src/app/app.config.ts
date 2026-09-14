726621d118c9480aad38447acf313c663ea5da6a
Manfred Steyer <manfred.steyer@gmx.net>
2026-09-14 14:49:50 +0200

    feat: solve a2ui lab

--- projects/a2ui-demo/src/app/app.config.ts
+++ projects/a2ui-demo/src/app/app.config.ts
@@ -1,21 +1,16 @@
-import {
-  BasicCatalog,
-  provideA2Ui,
-  provideMarkdownRenderer,
-} from '@a2ui/angular/v0_9';
+import { provideA2Ui, provideMarkdownRenderer } from '@a2ui/angular/v0_9';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { marked } from 'marked';
 
+import { customCatalog } from './custom-catalog/custom-catalog';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideA2Ui({
-      // TODO: Register custom catalog i/o BasicCatalog
-      catalogs: [new BasicCatalog()],
-    }),
+    provideA2Ui({ catalogs: [customCatalog] }),
     provideMarkdownRenderer(async (markdown) =>
       marked.parse(String(markdown ?? '')),
     ),
--- projects/a2ui-demo/src/app/app.ts
+++ projects/a2ui-demo/src/app/app.ts
@@ -45,10 +45,24 @@ export class App {
       (action: A2uiClientAction) => {
         console.log('[A2UI Event]', action);
 
-        // TODO: Add handler for increaseMiles action:
-        //       1. Get the passenger from the action context
-        //       2. Update its bonusMiles by 300 and write it back
-        // Hint: For writing back, use renderer.processMessages
+        if (action.name !== 'increaseMiles') {
+          return;
+        }
+
+        const passenger = action.context as Passenger;
+        this.renderer.processMessages([
+          {
+            version: 'v0.9',
+            updateDataModel: {
+              surfaceId: this.surfaceId,
+              path: '/passenger',
+              value: {
+                ...passenger,
+                bonusMiles: passenger.bonusMiles + 300,
+              },
+            },
+          },
+        ]);
       },
     );
 
